c270dcbb740d71377514902e52d543fdb14c1815
soysauce2393 <hubblesoren@gmail.com>
2024-02-11 18:34:13 -0800

    now it automatically calibrates the shooter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,8 @@ import com.ctre.phoenix6.signals.SensorDirectionValue;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -22,6 +23,7 @@ import frc.robot.subsystems.Telemetry;
 
 import frc.robot.subsystems.*;
 import frc.robot.commands.*;
+
 
 public class RobotContainer {
 
@@ -53,10 +55,9 @@ public class RobotContainer {
 
   private final Telemetry logger = new Telemetry(MaxSpeed);
 
-  
 
   private void configureBindings() {
-    
+
     // SWERVE BINDS
     drivetrain.setDefaultCommand(
         drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
