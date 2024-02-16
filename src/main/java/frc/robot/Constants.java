fbe10bca764b640cbd0b131d953b789fb9c6448d
soysauce2393 <hubblesoren@gmail.com>
2024-02-15 23:05:20 -0800

    cleaned up constants a bit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,26 +24,21 @@ public class Constants {
     public static final int kShooterDistanceSensorID = 1;
     public static final int kShooterDistanceSensorTreshold = 100;
     public static final int kShooterLimitSwitchID = 9;
-    public static final double kLimitSwitchToZero = -13;
 
-    public static final int kLeftWristID = 30;
-    public static final double kShooterHandoffPosition = 0;
-    public static final double kShooterAmpPosition = 0;
+    public static final int kLeftWristID = 30; //
+    public static final double kShooterHandoffPosition = 4; //
+    public static final double kShooterAmpPosition = 0; // not tuned yet
 
     public static final double kShooterSpinSpeed = 1; // speed that flywheels shoot at
     public static final double kShooterIntakeSpeed = 0.8; // speed that shooter intake wheels spin at
-    public static final double kShooterAmpSpeed = -1;
-    public static final double kShooterSpeakerPosition = 0;
+    public static final double kShooterAmpSpeed = -1; // intake wheels on the shooter speed
 
     // intake constants
-    public static final int kIntakeRollerID = 24;
+    public static final int kIntakeRollerID = 24; 
     public static final int kIntakePivotID = 25;
 
     public static final int kIntakeDistanceSensorID = 0;
     public static final double kIntakeDistanceSensorThreshold = 300.0;
-
-    public static final int kIntakeEncoderID = 0;
-    public static final double kIntakeEncoderOffset = 0.0;
 
     public static final double kIntakeDownPosition = 6.8;
     public static final double kIntakeHandoffPosition = 0.0;
