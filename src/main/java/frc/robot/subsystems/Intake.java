f45e157aae19d0bac8f3544002466ea2a312f97d
soysauce2393 <hubblesoren@gmail.com>
2024-02-17 23:09:54 -0800

    using profiledPIDController

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,11 +7,14 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.mechanisms.swerve.utility.PhoenixPIDController;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,43 +22,44 @@ import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
 
-  // need to switch to https://api.ctr-electronics.com/phoenix6/release/java/com/ctre/phoenix6/mechanisms/swerve/utility/PhoenixPIDController.html
-  PhoenixPIDController controller = new PhoenixPIDController(0, 0, 0);
+
   /** Creates a new intake. */
   TalonFX intakeRoller = new TalonFX(Constants.kIntakeRollerID);
   TalonFX intakePivot = new TalonFX(Constants.kIntakePivotID);
   DigitalInput zeroSwitch = new DigitalInput(Constants.kIntakeLimitSwitchID);
   //CANcoder intakeEncoder = new CANcoder(Constants.kIntakeEncoderID);
   AnalogInput distanceSensor = new AnalogInput(Constants.kIntakeDistanceSensorID);
-
-  private final PositionVoltage m_voltagePosition;
+  double lastSpeed;
+  double lastTime;
+  ProfiledPIDController controller;
+  ArmFeedforward feedforward;
 
 
   public Intake() {
-    m_voltagePosition = new PositionVoltage(0, 0, true, 0, 0, false, false, false);
     distanceSensor.setAverageBits(4);
     
     intakeRoller.setNeutralMode(NeutralModeValue.Brake);
     intakePivot.setNeutralMode(NeutralModeValue.Brake);
-
-    //pivot PID
-    TalonFXConfiguration configs = new TalonFXConfiguration();
-    configs.Slot0.kP = 1.4;
-    configs.Slot0.kD = 0.35;
-    configs.Slot0.kI = 0.01;
-
-
-    intakePivot.getConfigurator().apply(configs);
+   
     intakePivot.setPosition(0);
-    intakePivot.setControl(m_voltagePosition.withPosition(0));
+
+    lastSpeed = 0;
+    lastTime = Timer.getFPGATimestamp();
+
+    feedforward = new ArmFeedforward(0, 0, 0);
+
+     controller = new ProfiledPIDController(
+      0, 0, 0,
+      new TrapezoidProfile.Constraints(5, 10)
+    );
+
+
   }
 
+  public void setAngle(double goalPosition) {
+    controller.setGoal(goalPosition);
+  } 
 
-
-  public void setAngle(double angle){
-    
-    intakePivot.setControl(m_voltagePosition.withPosition(angle));
-  }
 
   public void setSpeed(double speed){
     intakeRoller.set(speed);
@@ -79,7 +83,12 @@ public class Intake extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    double acceleration = (controller.getSetpoint().velocity - lastSpeed) / (Timer.getFPGATimestamp() - lastTime);
+    intakePivot.setVoltage(
+        controller.calculate(intakeAngle())
+        + feedforward.calculate(controller.getSetpoint().velocity, acceleration));
+    lastSpeed = controller.getSetpoint().velocity;
+    lastTime = Timer.getFPGATimestamp();
   }
 
 }
