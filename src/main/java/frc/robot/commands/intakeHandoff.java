c33aa4e0bf691fa0e57dcb82c38f3399cd1b2be4
soysauce2393 <hubblesoren@gmail.com>
2024-02-19 20:38:27 -0800

    print points for debugging

--- src/main/java/frc/robot/commands/intakeHandoff.java
+++ src/main/java/frc/robot/commands/intakeHandoff.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -18,6 +19,11 @@ import frc.robot.subsystems.Shooter;
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class intakeHandoff extends SequentialCommandGroup {
   /** Creates a new intakeHandoff. */
+  private int cmdNum = 0;
+  private Command printPoint(){
+    cmdNum ++;
+    return new InstantCommand(()->System.out.println(cmdNum));
+  }
   public intakeHandoff(Shooter shooter, Intake intake) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
@@ -25,21 +31,37 @@ public class intakeHandoff extends SequentialCommandGroup {
     addCommands(
       /*new SequentialCommandGroup(
       ),*/
+        printPoint(),
         new setShooterAngle(shooter, Constants.kShooterHandoffPosition),
+        printPoint(),
         new setIntakeAngle(intake, Constants.kIntakeDownPosition-3),
+        printPoint(),
         new setIntakeSpeed(intake, -0.3),
+        printPoint(),
         new WaitUntilCommand(intake::hasNote),
+        printPoint(),
         new setIntakeSpeed(intake, 0),
+        printPoint(),
         new setIntakeAngle(intake, 0.2),
+        printPoint(),
         new WaitUntilCommand(intake::isAtGoal),
+        printPoint(),
         new setShooterIntakeSpeed(shooter, Constants.kShooterIntakeSpeed),
+        printPoint(),
         new setIntakeSpeed(intake, 0.4),
+        printPoint(),
         new WaitUntilCommand(shooter::hasNote), //Problems start here
+        printPoint(),
         new setShooterIntakeSpeed(shooter, -0.3),
+        printPoint(),
         new setIntakeSpeed(intake, 0.3),
+        printPoint(),
         new WaitCommand(1),
+        printPoint(),
         new setShooterIntakeSpeed(shooter, 0),
-        new setIntakeSpeed(intake, 0)
+        printPoint(),
+        new setIntakeSpeed(intake, 0),
+        printPoint()
     );
   }
 }
