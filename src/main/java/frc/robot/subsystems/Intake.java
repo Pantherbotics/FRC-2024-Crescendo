919d9bed3934b027fa358be7d78017bbd1c51093
Jai <jaiblueskyes@gmail.com>
2024-02-20 23:34:06 -0800

    added test routines to intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,19 +4,40 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.units.Angle;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.MutableMeasure;
+import edu.wpi.first.units.Velocity;
+import edu.wpi.first.units.Voltage;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants;
 
-public class Intake extends SubsystemBase {
+import static edu.wpi.first.units.MutableMeasure.mutable;
+
+public class Intake extends SubsystemBase {  
+
+ // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
+  private final MutableMeasure<Voltage> appliedVoltage = mutable(Volts.of(0));
+  // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
+  private final MutableMeasure<Angle> angle = mutable(Rotations.of(0));
+  // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
+  private final MutableMeasure<Velocity<Angle>> velocity = mutable(RotationsPerSecond.of(0));
+  //this way seems more convoluted than just reallocating, but its in the wpilib examples.
 
 
   /** Creates a new intake. */
@@ -25,6 +46,42 @@ public class Intake extends SubsystemBase {
   DigitalInput zeroSwitch = new DigitalInput(Constants.kIntakeLimitSwitchID);
   //CANcoder intakeEncoder = new CANcoder(Constants.kIntakeEncoderID);
   AnalogInput distanceSensor = new AnalogInput(Constants.kIntakeDistanceSensorID);
+
+  private final SysIdRoutine pivotSysIdRoutine =
+      new SysIdRoutine(
+          new SysIdRoutine.Config(),
+          new SysIdRoutine.Mechanism(
+              (Measure<Voltage> volts) -> {
+                intakePivot.setVoltage(volts.magnitude());
+              },
+              log -> {
+                log.motor("intake-pivot")
+                    .voltage(
+                        appliedVoltage.mut_replace(
+                            intakePivot.get() * RobotController.getBatteryVoltage(), Volts))
+                    .angularPosition(angle.mut_replace(intakePivot.getPosition().getValueAsDouble(), Rotations))
+                    .angularVelocity(
+                        velocity.mut_replace(intakePivot.getVelocity().getValueAsDouble(), RotationsPerSecond));
+              },
+              this));
+  
+  private final SysIdRoutine rollerSysIdRoutine =
+      new SysIdRoutine(
+          new SysIdRoutine.Config(),
+          new SysIdRoutine.Mechanism(
+              (Measure<Voltage> volts) -> {
+                intakeRoller.setVoltage(volts.magnitude());
+              },
+              log -> {
+                log.motor("intake-roller")
+                    .voltage(
+                        appliedVoltage.mut_replace(
+                            intakeRoller.get() * RobotController.getBatteryVoltage(), Volts))
+                    .angularPosition(angle.mut_replace(intakeRoller.getPosition().getValueAsDouble(), Rotations))
+                    .angularVelocity(
+                        velocity.mut_replace(intakeRoller.getVelocity().getValueAsDouble(), RotationsPerSecond));
+              },
+              this));
 
   public Intake() {
     distanceSensor.setAverageBits(4);
@@ -78,5 +135,21 @@ public class Intake extends SubsystemBase {
 
   public Command setZero(){
     return new RunCommand(()->intakePivot.setPosition(0));
+  }
+
+  public Command pivotSysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return pivotSysIdRoutine.quasistatic(direction);
+  }
+
+   public Command pivotSysIdDynamic(SysIdRoutine.Direction direction) {
+    return pivotSysIdRoutine.dynamic(direction);
+  }
+
+   public Command rollerSysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return rollerSysIdRoutine.quasistatic(direction);
+  }
+
+   public Command rollerSysIdDynamic(SysIdRoutine.Direction direction) {
+    return rollerSysIdRoutine.dynamic(direction);
   }
 }
