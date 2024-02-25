bc81eaef24cd30ea451573bda9edffdc093f1163
soysauce2393 <hubblesoren@gmail.com>
2024-02-25 10:50:37 -0800

    speakerPose is now based on alliance color

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import java.util.Optional;
+
 import com.pathplanner.lib.path.PathConstraints;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -11,6 +13,9 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+
 
 /** Add your docs here. */
 public class Constants {
@@ -63,16 +68,29 @@ public class Constants {
     public static final Transform3d kRobotToCamera = new Transform3d(0,-0.4,1.3,new Rotation3d(0,0,0));
 
     // pathfinding
+    private Optional<Alliance> ally = DriverStation.getAlliance();
+    public static Pose2d kSpeakerPose;
 
+    public Constants(){
+        if (ally.isPresent()){
+            if (ally.get() == Alliance.Red){
+                kSpeakerPose = new Pose2d(16.3, 5.5, Rotation2d.fromDegrees(180));
+            } else {
+                kSpeakerPose = new Pose2d(0.25, 5.5, Rotation2d.fromDegrees(180));
+            }
+        } else {
+            kSpeakerPose = new Pose2d(16.3, 5.5, Rotation2d.fromDegrees(180));
+        }
+    }
+    
     public static final Pose2d kAmpPose = new Pose2d(2.3, 7, Rotation2d.fromDegrees(90));
     public static final Pose2d kSourcePose = new Pose2d(14.5,1.4,Rotation2d.fromDegrees(-30));
-    public static final Pose2d kSpeakerPose = new Pose2d(0.2, 5.5, Rotation2d.fromDegrees(180));
     public static final PathConstraints kPathfindingConstraints = 
         new PathConstraints(
             3.0, 4.0,
             Units.degreesToRadians(540), Units.degreesToRadians(720)
         );
-
+    
 
 
 }
