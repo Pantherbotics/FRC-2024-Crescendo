7b9fd4654a8d46ca9c1326b88dfe7c9e6c4bcd28
soysauce2393 <hubblesoren@gmail.com>
2024-02-27 20:21:27 -0800

    cleaned up vision

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -13,58 +13,38 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
-import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
-import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain.SwerveDriveState;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
-  //public final PhotonCamera mainCam;
+  public final PhotonCamera mainCam;
   public final PhotonCamera sideCam;
-  //public final PhotonCamera backCam;
+  public final PhotonCamera backCam;
   public AprilTagFieldLayout tagLayout;
-  public Transform3d camToTarget = new Transform3d();
-  //public PhotonPoseEstimator mainPoseEstimator;
+  public PhotonPoseEstimator mainPoseEstimator;
   public PhotonPoseEstimator sidePoseEstimator;
-  //public PhotonPoseEstimator backPoseEstimator;
-  private Pose2d returnedPose;
-  private SwerveDriveState swerveState;
-  //private Optional<EstimatedRobotPose> mainEstimated;
+  public PhotonPoseEstimator backPoseEstimator;
+  private Optional<EstimatedRobotPose> mainEstimated;
   private Optional<EstimatedRobotPose> sideEstimated;
-  //private Optional<EstimatedRobotPose> backEstimated;
+  private Optional<EstimatedRobotPose> backEstimated;
   private CommandSwerveDrivetrain swerve;
 
 
 
   public Vision(CommandSwerveDrivetrain swerve) {
-    //mainCam = new PhotonCamera(Constants.kMainCameraName);
+    mainCam = new PhotonCamera(Constants.kMainCameraName);
     sideCam = new PhotonCamera(Constants.kSideCameraName);
-    //backCam = new PhotonCamera(Constants.kBackCameraName);
+    backCam = new PhotonCamera(Constants.kBackCameraName);
     this.swerve = swerve;
     tagLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
-    //mainPoseEstimator = new PhotonPoseEstimator(tagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, mainCam, Constants.kRobotToMainCam);
+    mainPoseEstimator = new PhotonPoseEstimator(tagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, mainCam, Constants.kRobotToMainCam);
     sidePoseEstimator = new PhotonPoseEstimator(tagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, sideCam, Constants.kRobotToSideCam);
-    //backPoseEstimator = new PhotonPoseEstimator(tagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, backCam, Constants.kRobotToBackCam);
+    backPoseEstimator = new PhotonPoseEstimator(tagLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, backCam, Constants.kRobotToBackCam);
 
   }
-
-  public double getYaw(PhotonCamera cam){
-
-    if (hasTarget(cam)){
-      return cam.getLatestResult().getBestTarget().getYaw();
-    }
-    return 0;
-
-  }
-
 
   public boolean hasTarget(PhotonCamera cam){
 
@@ -74,28 +54,20 @@ public class Vision extends SubsystemBase {
   }
 
   public void updatePose(){
-
-    //swerveState = swerve.getState();
-    //photonPoseEstimator.setReferencePose(swerveState.Pose);
-    //mainEstimated = mainPoseEstimator.update();
+    mainEstimated = mainPoseEstimator.update();
     sideEstimated = sidePoseEstimator.update();
-    //backEstimated = backPoseEstimator.update();
+    backEstimated = backPoseEstimator.update();
 
-    /*if (mainEstimated.isPresent()){
+    if (mainEstimated.isPresent()){
       swerve.addVisionMeasurement(mainEstimated.get().estimatedPose.toPose2d(), mainEstimated.get().timestampSeconds);
-    }*/
+    }
     if (sideEstimated.isPresent()){
       swerve.addVisionMeasurement(sideEstimated.get().estimatedPose.toPose2d(), sideEstimated.get().timestampSeconds);
     }
-    /*if (backEstimated.isPresent()){
+    if (backEstimated.isPresent()){
       swerve.addVisionMeasurement(backEstimated.get().estimatedPose.toPose2d(), backEstimated.get().timestampSeconds);
-    }*/
+    }
   }
-
-  public Command update(Vision vision, SwerveDrivetrain swerve){
-    return new InstantCommand(() -> updatePose());
-  }
-
 
   @Override
   public void periodic() {
